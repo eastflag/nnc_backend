cae2a34bc50aee38a991735933c4ccff52ff2abc
DK <eastflag@gmail.com>
2024-04-24 01:19:26 +0900

    first commit

---
+++ src/main/java/com/eastflag/nnc/NncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eastflag.nnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NncBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eastflag/nnc/NncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastflag.nnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
