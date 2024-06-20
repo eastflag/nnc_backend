3019067b153594ff1eafe9ef21f883ee1e624807
A75178 <eastflag@cnspartner.com>
2024-06-20 15:42:37 +0900

    log4j2 설정 추가

--- src/main/java/com/eastflag/nnc/demo/DemoController.java
+++ src/main/java/com/eastflag/nnc/demo/DemoController.java
@@ -1,17 +1,26 @@
 package com.eastflag.nnc.demo;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1")
 public class DemoController {
 
-  @GetMapping("/demo")
-  public ResponseEntity<String> sayHello() {
-    return ResponseEntity.ok("Hello from secured endpoint");
+  @GetMapping("/auth/demo")
+  public ResponseEntity<String> authDemo() {
+    log.info("path: {}", "/api/v1/auth/demo");
+    return ResponseEntity.ok("Hello from Method...");
   }
 
+
+  @GetMapping("/user/demo")
+  public ResponseEntity<String> userDemo() {
+    log.info("path: {}", "/api/v1/user/demo");
+    return ResponseEntity.ok("Hello from /api/vi/user/demo");
+  }
 }
