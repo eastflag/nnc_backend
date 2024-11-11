7cfa053029665aa6c3412e0f233e1d93852f5873
A75178 <eastflag@cnspartner.com>
2024-11-11 16:12:07 +0900

    .

--- src/main/java/com/eastflag/nnc/board/dto/BoardSearchDto.java
+++ src/main/java/com/eastflag/nnc/board/dto/BoardSearchDto.java
@@ -1,10 +1,11 @@
 package com.eastflag.nnc.board.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class BoardSearchDto {
     private String title;
-
+    @JsonProperty("category_name")
     private String categoryName;
 }
